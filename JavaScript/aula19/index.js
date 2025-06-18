bdbde35a1393e460547f6c7ee7473b7162e2265f
Carlos Herik <carlosherik.dev@gmail.com>
2025-06-17 22:26:37 -0300

    Create index.js

---
+++ JavaScript/aula19/index.js
@@ -0,0 +1,6 @@
+// operação ternária
+// (condição) ? 'valor para verdadeiro' : 'valor para falso'
+
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
