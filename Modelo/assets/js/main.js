a3313db343cc7ba2ba8f8cd0e9fa32da9fff96f2
Carlos Herik <carlosherik.dev@gmail.com>
2025-06-17 21:29:12 -0300

    Tabela IMC (JavaScript)

--- Modelo/assets/js/main.js
+++ Modelo/assets/js/main.js
@@ -18,10 +18,36 @@ form.addEventListener('submit', function (event)  {
   }
 
   const imc = getImc(peso, altura);
+  const nivelImc = getNinelImc(imc);
 
-  console.log(imc);
+  const mensagem = `Seu IMC é ${imc} (${nivelImc}).`;
+
+  setResultado(mensagem, true);
 
 });
+
+function getNinelImc (imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+  if (imc >= 39.9) {
+    return nivel [5];
+  }
+    if (imc >= 34.9) {
+    return nivel [4];
+  }
+    if (imc >= 29.9) {
+    return nivel [3];
+  }
+    if (imc >= 24.9) {
+    return nivel [2];
+  }
+    if (imc >= 18.5) {
+    return nivel [1];
+  }
+    if (imc < 18.5) {
+    return nivel [0];
+  }
+}
 
 function getImc (peso, altura) {
   const imc = peso / altura ** 2;
@@ -38,6 +64,9 @@ function setResultado (mensagem, isValid) {
   resultado.innerHTML = '';
 
   const p = criaP();
+
+  if (isValid) p.classList.add();
+
   p.innerHTML = mensagem;
   resultado.appendChild(p);
    
