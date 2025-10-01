8498ad39f0a9ad584091666020d461ec3e5df17d
Carlos Herik <carlosherik.dev@gmail.com>
2025-10-01 19:08:21 -0300

    Avançando no JS

---
+++ CursoSujeitoProgramador/Aulas Javascript/aula08/script.js
@@ -0,0 +1,17 @@
+// setInterval
+
+/*function acao(){
+  document.write("Executando... <br>")
+}
+
+setInterval(acao, 5000) */
+
+/*setInterval(() => {
+  document.write("Executando!!! <br>")
+},1000) */
+
+function acao(){
+  document.write("Executando... <br>")
+}
+
+setTimeout(acao, 3000);
