ebbaad133c36513b6344d420f5795ffe8a93b79b
C-Herik-Dev <carlosherik.dev@gmail.com>
2026-01-05 18:00:38 -0300

    Projeto
    Projeto de criptomoedas

---
+++ Estudos-Projetos/Projetos/projeto-criptomoedas/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { router } from './router'
+import { RouterProvider } from 'react-router'
+
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ Estudos-Projetos/Projetos/projeto-criptomoedas/src/pages/detail/index.tsx
@@ -0,0 +1,10 @@
+
+
+export function Detail() {
+
+  return (
+    <div>
+      <h2>Detalhes da Criptomoeda</h2>
+    </div>
+  )
+}
