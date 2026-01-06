94a860be8b6b9f09e6d8e11b235a51d7d51318b7
C-Herik-Dev <carlosherik.dev@gmail.com>
2026-01-05 22:47:36 -0300

    Add detail page and enhance home page for cryptocurrencies
    Implemented a new detail page for individual cryptocurrencies with API integration and responsive styling. Improved the home page to fetch and display paginated cryptocurrency data, added a search form, and enhanced UI with new styles and icons. Removed unused Cryptomoedas.png asset.

--- Estudos-Projetos/Projetos/projeto-criptomoedas/src/pages/home/index.tsx
+++ Estudos-Projetos/Projetos/projeto-criptomoedas/src/pages/home/index.tsx
@@ -1,14 +1,96 @@
+import { useState, useEffect ,type FormEvent } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
+
+export interface CoinProps{
+  id: string;
+  name: string;
+  symbol: string;
+  marketCapUsd: string;
+  priceUsd: string;
+  volumeUsd24Hr: string;
+  changePercent24Hr: string;
+  supply: string;
+  maxSupply: string;
+  vwap24Hr: string;
+  rank: string;
+  explorer: string;
+  formatedPrice?: string;
+  formatedMarket?: string;
+  formatedVolume?: string;
+}
+
+interface DataProp{
+  data: CoinProps[];
+}
  
 export function Home() {
+  const [input, setInput] = useState('')
+  const [coins, setCoins] = useState<CoinProps[]>([])
+  const [offset, setOffset] = useState(0)
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, [offset])
+
+  async function getData(){
+    fetch(`https://rest.coincap.io/v3/assets?limit=10&offset=${offset}&apiKey=7d783cf95c1901061031fb0e4a4f0b8e63b307129df218fb35846ca8920ebbc6`)
+    .then((response) => response.json())
+    .then((data: DataProp) => {
+      const coinsData = data.data;
+
+      const price = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      })
+      const priceCompact = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        notation: "compact"
+      })
+      
+      const formattedCoins = coinsData.map((coin) => {
+        const formatted = {
+          ...coin,
+          formatedPrice: price.format(Number(coin.priceUsd)),
+          formatedMarket: priceCompact.format(Number(coin.marketCapUsd)),
+          formatedVolume: priceCompact.format(Number(coin.volumeUsd24Hr)),
+        }
+        return formatted;
+      })
+
+      const listCoins = [...coins, ...formattedCoins];
+      setCoins(listCoins);
+      
+    })
+}
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault();
+    if(input === "") return;
+    navigate(`/detail/${input}`);
+    setInput('');
+  }
+
+  function handleGetMore(){
+    if(offset === 0){
+      setOffset(10);
+      return;
+    }
+    setOffset(offset + 10);
+  }
+
 
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input type="text"
         placeholder="Digite o nome da criptomoeda"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
          />
         <button type="submit">
           <BsSearch size={30} color="#fff" />
@@ -27,34 +109,49 @@ export function Home() {
         </thead>
 
         <tbody id="tbody">
-          <tr className={styles.tr}>
+
+          {coins.length > 0 && coins.map( (item) => (
+            <tr className={styles.tr} key={item.id}>
             <td className={styles.tdLabel} data-label="Moeda">
+
               <div className={styles.name}>
-                <Link to={"/detail/bitcoin"}>
-                  <span>Bitcoin</span> | BTC
+                <img
+                  className={styles.coinIcon}
+                  alt="logo Criptomoeda"
+                  src={`https://assets.coincap.io/assets/icons/${item.symbol.toLocaleLowerCase()}2@2x.png` && `https://assets.coincap.io/assets/icons/${item.symbol.toLocaleLowerCase()}@2x.png` }
+                />
+                <Link to={`/detail/${item.id}`}>
+                  <span>{item.name}</span> | {item.symbol}
                 </Link>
               </div>
             </td>
             
             <td className={styles.tdLabel} data-label="Valor mercado">
-              1T
+              {item.formatedMarket}
             </td>
 
             <td className={styles.tdLabel} data-label="Preço">
-              8.000
+              {item.formatedPrice}
             </td>
 
             <td className={styles.tdLabel} data-label="Valor mercado">
-              2B
+              {item.formatedVolume}
             </td>
 
-            <td className={styles.tdLabel} data-label="Mudança 24h">
-              <span>1.2%</span>
+            <td className={Number(item.changePercent24Hr) > 0 ? styles.tdProfit : styles.tdLoss} data-label="Mudança 24h">
+              <span>{Number(item.changePercent24Hr).toFixed(2)}%</span>
             </td>
-          </tr>
+            </tr>
+          ))}
 
         </tbody>  
       </table>
+
+      <button className={styles.buttonMore} 
+      onClick={handleGetMore}
+      >
+        Ver mais moedas...
+      </button>
 
     </main>
   )
