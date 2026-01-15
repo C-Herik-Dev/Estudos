cb410b0bd6813bf8f791854e9028d385fb0cf363
C-Herik-Dev <carlosherik.dev@gmail.com>
2026-01-15 18:44:25 -0300

    New miniProject: Devshop (projeto pratico para aprender context API)

---
+++ Estudos-Projetos/CursoSujeitoProgramador/Mini-Projetos/Projeto-estudo-Devshop/src/App.tsx
@@ -0,0 +1,23 @@
+import { Cart } from './pages/cart'
+import { Home } from './pages/home'
+import { Layout } from './components/layout'
+
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+      element: <Home/>
+      },
+      {
+        path: "cart",
+        element: <Cart/>
+      }
+    ]
+  }
+])
+
+export { router }
