32fce001cac6b7de939b1e6829c1a26ebd6bd362
C-Herik-Dev <carlosherik.dev@gmail.com>
2026-01-16 20:24:22 -0300

    inicializando primeiros passos no miniprojeto

--- Estudos-Projetos/CursoSujeitoProgramador/Mini-Projetos/Projeto-estudo-Devshop/src/pages/cart/index.tsx
+++ Estudos-Projetos/CursoSujeitoProgramador/Mini-Projetos/Projeto-estudo-Devshop/src/pages/cart/index.tsx
@@ -1,7 +1,32 @@
 export function Cart(){
   return (
-    <div>
-      <h1>Pagina do Carrinho</h1>
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+      <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img className="w-28"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZJP8i_HT-mrVWOl-J_ZPbwrxyPQKVf-Q_xw&s"
+          alt='Logo produto'
+        />
+
+        <strong>Preço: R$ 500</strong>
+
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            -
+          </button>
+          2
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            +
+          </button>
+        </div>
+
+        <strong className="float-right">
+          SubTotal: R$ 1.000
+        </strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: R$ 1.000</p>
     </div>
   )
 }
--- Estudos-Projetos/CursoSujeitoProgramador/Mini-Projetos/Projeto-estudo-Devshop/src/pages/home/index.tsx
+++ Estudos-Projetos/CursoSujeitoProgramador/Mini-Projetos/Projeto-estudo-Devshop/src/pages/home/index.tsx
@@ -1,8 +1,31 @@
+import { BsCartPlus } from 'react-icons/bs'
 
 export function Home(){
   return (
     <div>
-      <h1>Pagina home</h1>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-5">
+          <section className='w-full lg:w-auto'>
+            <img
+              className='w-full rounded-lg max-h-70 mb-2'
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZJP8i_HT-mrVWOl-J_ZPbwrxyPQKVf-Q_xw&s"
+              alt="Logo do produto"
+            />
+            <p className='font-medium mt-1 mb-2'>Teclado Gamer</p>
+
+            <div className='flex gap-3 items-center'>
+              <strong className='text-zinc-700/90'>
+                R$ 500
+              </strong>
+              <button className='bg-zinc-900 p-1 rounded'>
+                <BsCartPlus size={20} color="#fff"/>
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
