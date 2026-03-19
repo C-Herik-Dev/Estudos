f7ff825a32e7c1a858923f633eb4a5c76e119668
C-Herik-Dev <carlosherik.dev@gmail.com>
2026-03-19 19:40:45 -0300

    feat: conectando firebase e criacao de context de autenticacao

--- Estudos-Projetos/Projetos/AutoHub/src/pages/login/index.tsx
+++ Estudos-Projetos/Projetos/AutoHub/src/pages/login/index.tsx
@@ -1,10 +1,14 @@
 import logoImg from '../../assets/logo.svg'
 import { Container } from '../../components/container/index'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Input } from '../../components/input'
 import { useForm} from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { auth } from '../../services/firebaseConnection'
+import {useEffect} from "react"
+
 
 const schema = z.object({
   email: z.email("Insira um email válido.").nonempty("O campo email é obrigatório."),
@@ -14,13 +18,28 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>
 
 export function Login() {
+  const navigate = useNavigate();
   const {register, handleSubmit, formState: {errors }} = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
   })
 
-  function onSubmit(data: FormData){
-    console.log(data)
+  useEffect(()=> {
+    async function handleLogout(){
+      await signOut(auth)
+    }
+    handleLogout();
+  },[])
+
+ function onSubmit(data: FormData){
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then(() =>{
+      console.log("logado com sucesso!")
+      navigate("/dashboard", {replace: true})
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
   }
   return (
     <Container>
---
+++ Estudos-Projetos/Projetos/AutoHub/src/services/firebaseConnection.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from "firebase/storage"
+const firebaseConfig = {
+  apiKey: "AIzaSyDVcj_HVDgdZQmG24cgT5AEP_4fYFg5RaI",
+  authDomain: "autohub-c1b23.firebaseapp.com",
+  projectId: "autohub-c1b23",
+  storageBucket: "autohub-c1b23.firebasestorage.app",
+  messagingSenderId: "341731600231",
+  appId: "1:341731600231:web:8a1ae7055b3f8fcaf471c2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export {db, auth, storage}
